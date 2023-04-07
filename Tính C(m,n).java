d6d9046581a6ccefd74b4264ab0b45e8af30cf80
Dvp9402 <128269036+Dvp9402@users.noreply.github.com>
2023-04-07 23:44:55 +0700

    Add files via upload

---
+++ Tính C(m,n).java
@@ -0,0 +1,56 @@
+package Function_Oriented;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Bai04 {
+	// Phương thức nhập vào 1 số
+	public static int input(Scanner sc) {
+		int n = 0;
+		boolean check = true;
+		while (check) {
+			try {
+				n = sc.nextInt();
+				if (n < 0)
+					System.out.print("Bạn phải nhập 1 số nguyên >= 0.\nNhập lại: ");
+				else
+					check = false;
+			} catch (InputMismatchException e) {
+				System.out.print("Bạn phải nhập 1 số nguyên >= 0.\nNhập lại: ");
+				sc.nextLine();
+			}
+		}
+		return n;
+	}
+
+	// Phương thức tính tổ hợp
+	public static long combination(int n, int m) {
+		if (m == 0 || m == n)
+			return 1;
+		else {
+			long a = 1, b = 1;
+			for (int i = m; i <= n; i++)
+				a *= i;
+			for (int i = 1; i <= n - m; i++)
+				b *= i;
+			return a/b;
+		}
+	}
+
+	// Main
+	public static void main(String[] args) {
+		System.out.println("Tính C(m,n)");
+		System.out.println("-------------------------------------------------");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhập số nguyên m (m >= 0): ");
+		int m = input(sc);
+		System.out.print("Nhập số nguyên n (n >= " + m + "): ");
+		int n = input(sc);
+		while(n < m) {
+			System.out.print("Bạn phải nhập n lớn hơn " + m + "\nNhập lại: ");
+			n = input(sc);
+		}
+		sc.close();
+		System.out.println("-------------------------------------------------");
+		System.out.println("C(" + m + "," + n + ") = " + combination(n,m));
+	}
+}
---
+++ UCLN.java
@@ -0,0 +1,51 @@
+package Function_Oriented;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Bai05 {
+	// Phương thức nhập vào 1 số
+	public static int input(Scanner sc) {
+		int n = 0;
+		boolean check = true;
+		while (check) {
+			try {
+				n = sc.nextInt();
+				check = false;
+			} catch (InputMismatchException e) {
+				System.out.print("Bạn phải nhập 1 số nguyên.\nNhập lại: ");
+				sc.nextLine();
+			}
+		}
+		return n;
+	}
+
+	// Phương thức trả về ước chung lớn nhất của 2 số nguyên khác 0
+	public static int greatest_common_divisor(int a, int b) {
+		if (b == 0)
+			return a;
+		int temp = a % b;
+		while (temp != 0) {
+			a = b;
+			b = temp;
+			temp = a % b;
+		}
+		return Math.abs(b);
+	}
+
+	// Main
+	public static void main(String[] args) {
+		System.out.println("Tìm ước chung lớn nhất của 2 số a và b");
+		System.out.println("-------------------------------------------------");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhập số nguyên a: ");
+		int a = input(sc);
+		System.out.print("Nhập số nguyên b: ");
+		int b = input(sc);
+		sc.close();
+		System.out.println("-------------------------------------------------");
+		if (a == 0 && b == 0)
+			System.out.println("Không có ước chung lớn nhất");
+		else
+			System.out.println("Ước chung lớn nhất của " + a + " và " + b + " là: " + greatest_common_divisor(a, b));
+	}
+}
