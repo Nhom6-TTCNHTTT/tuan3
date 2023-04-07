4981ad9e7417e044abdc64a100b6629dfb8c1bbe
NuV00 <128266226+NuV00@users.noreply.github.com>
2023-04-08 00:23:48 +0700

    Update UCLN.java

--- UCLN.java
+++ UCLN.java
@@ -2,7 +2,7 @@ package Function_Oriented;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Bai05 {
+public class UCLN {
 	// Phương thức nhập vào 1 số
 	public static int input(Scanner sc) {
 		int n = 0;
